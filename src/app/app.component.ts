13bd127d0a8c4bf967f628c86b1d05d533fdd25f
Karhenpe <karhenpe@gmail.com>
2020-11-02 00:30:25 -0600

    Configuracion Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
